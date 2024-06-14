0d89e716763b37a0b14735031b906b3d3a10c5a2
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-06-14 10:09:29 -0300

    user disconnected

--- internal/infra/web/websocket/server/new.go
+++ internal/infra/web/websocket/server/new.go
@@ -75,8 +75,10 @@ func handleMessages() {
 			mu.Lock()
 			delete(verifiedDes, msg.Username)
 			mu.Unlock()
-			sendMessage(fmt.Sprintf("User %s connected", msg.Username), &messageConnnected)
+			//sendMessage(fmt.Sprintf("User %s connected", msg.Username), &messageConnnected)
 		}
+
+		sendMessage(fmt.Sprintf("User %s connected", msg.Username), &messageConnnected)
 
 		// mu.Lock()
 		// for _, user := range users {
